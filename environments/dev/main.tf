73736e72dc174be3f55dc31952e45efcbec24097
dsarraf <dsarraf@griddynamics.com>
2026-03-28 16:45:18 +0530

    extra feature added to terraform infra

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,6 +3,7 @@ module "networking" {
   vpc_name = "dev-vpc"
   ip_range = "10.0.1.0/24"
   region   = "us-central1"
+  environment = "dev"
 }
 
 module "storage" {
@@ -15,3 +16,5 @@ module "compute" {
   instance_name = "dev-vm"
   subnet_id     = module.networking.subnet_id # Connects to the network module
 }
+
+
