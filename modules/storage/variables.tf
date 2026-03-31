cf579c059f14a8faaa3d1ff6031fe762f7ce7971
dsarraf <dsarraf@griddynamics.com>
2026-03-31 16:33:05 +0530

    fixes the bug

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,2 +1,11 @@
 variable "bucket_name" {}
 variable "location"    { default = "US" }
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (dev or prod)"
+}
