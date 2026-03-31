3ac938aa9738e6af93d425d78d6a69d0ba576ffa
dsarraf <dsarraf@griddynamics.com>
2026-03-31 15:58:30 +0530

    added a storage bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket" "bucket" {
   location = var.location
   force_destroy = true
 }
+
+resource "google_storage_bucket" "backup_storage" {
+  name          = "backup-storage-${var.project_id}-${var.environment}"
+  location      = "US"
+  force_destroy = true # Good for demos so you can delete it easily later
+}
